b8a63aad574839d6e8d5d3549a5b22bd8615d050
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2024-07-11 11:27:35 +0200

    fix: example

--- example/index.ts
+++ example/index.ts
@@ -1,38 +1,27 @@
-import { Color, CustomBoard, King, Pawn } from "../src";
+import { Board } from "../src";
 import { capitalize, input, parseMoveInput, stringifyBoard } from "./helpers";
 
 const main = async () => {
   console.log("----------\nChess game\n----------\n");
   console.log('Input example: "a2 a4"');
 
-  const board = new CustomBoard(
-    [
-      new King("E1", Color.White),
-      new King("E8", Color.Black),
-      new Pawn("A2", Color.White),
-      new Pawn("B4", Color.Black),
-    ],
-    {
-      onCheck: (color) => {
-        console.log(`${capitalize(color)} king is in check!`);
-      },
-      onCheckMate: (color) => {
-        console.log(`${capitalize(color)} king is in checkmate!`);
-      },
-      onCheckResolve: () => {
-        console.log("Check resolved");
-      },
-      onStalemate: () => {
-        console.log(`Stalemate!`);
-      },
-      onBoardChange: (pieces) => {
-        console.log(`\n${stringifyBoard(pieces)}\n`);
-      },
-      onCapture: (_, __, position) => {
-        console.log(`${position.notation} is captured`);
-      },
+  const board = new Board({
+    onCheck: (color) => {
+      console.log(`${capitalize(color)} king is in check!`);
     },
-  );
+    onCheckMate: (color) => {
+      console.log(`${capitalize(color)} king is in checkmate!`);
+    },
+    onCheckResolve: () => {
+      console.log("Check resolved");
+    },
+    onStalemate: () => {
+      console.log(`Stalemate!`);
+    },
+    onBoardChange: (pieces) => {
+      console.log(`\n${stringifyBoard(pieces)}\n`);
+    },
+  });
 
   while (true) {
     const moveInput = await input("Enter your move: ");
